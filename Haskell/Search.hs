1735b85b555de5bb319dab803d18ca510e040771
G Faulkner <glyn.faulkner@york.ac.uk>
2014-06-03 13:48:22 +0100

    Updated references to datatypes in two more files

--- Haskell/Search.hs
+++ Haskell/Search.hs
@@ -1,71 +1,60 @@
 module Search where
 
+
 import Prelude hiding (lookup)
 import Data.List
 import Data.Maybe
 
 import ExAr
 import GPGraph
-import qualified Graph
+import Graph
 import GPSyntax
 
 
-source = Graph.source
-target = Graph.target
-eLabel = Graph.eLabel
-nLabel = Graph.nLabel
-allNodes = Graph.allNodes
-allEdges = Graph.allEdges
-nReLabel = Graph.nReLabel
-
-
-makeTestGraph n = nReLabel gr id (GPHostLabel [] Green)
+makeTestGraph n = nReLabel gr id (HostLabel [] Green)
     where
         gr = k n
         id = head $ allNodes gr
 
-makeRuleGraph = makeTestGraph
+--makeRuleGraph n = makeTestGraph
 
 testGraph = makeTestGraph 3
-searchFor = makeRuleGraph 2
+--searchFor = makeRuleGraph 2
 
-type GraphRuleNodeId = Graph.NodeId
-type GraphHostNodeId = Graph.NodeId
-
-type GraphRuleEdgeId = Graph.EdgeId
-type GraphHostEdgeId = Graph.EdgeId
-
-type GPRuleLabel = GPHostLabel
+type RuleNodeId = NodeId
+type HostNodeId = NodeId
+type RuleEdgeId = EdgeId
+type HostEdgeId = EdgeId
 
 type GraphMorphism = ( NodeMatches, EdgeMatches ) 
 type NodeMatches = [ NodeMatch ]
 type EdgeMatches = [ EdgeMatch ]
-type NodeMatch = (GraphRuleNodeId, GraphHostNodeId)
-type EdgeMatch = (GraphRuleEdgeId, GraphHostEdgeId)
+type NodeMatch = (RuleNodeId, HostNodeId)
+type EdgeMatch = (RuleEdgeId, HostEdgeId)
 
 
 -- two nodes are equal if their labels are equal
-nodesMatch :: GPHostGraph -> GPRuleGraph -> GraphHostNodeId -> GraphRuleNodeId -> Bool
+nodesMatch :: HostGraph -> RuleGraph -> HostNodeId -> RuleNodeId -> Bool
 nodesMatch g r gn rn = nLabel g gn == nLabel r rn
 
 -- two edges are equal if their labels are equal AND the nodes on either end are equal
-edgesMatch :: GPHostGraph -> GPRuleGraph -> GraphHostEdgeId -> GraphRuleEdgeId -> Bool
+edgesMatch :: HostGraph -> RuleGraph -> HostEdgeId -> RuleEdgeId -> Bool
 edgesMatch g r ge re = ( eLabel g ge == eLabel r re )
                        && ( nodesMatch g r (fromJust $ source g ge) (fromJust $ source r re) )
                        && ( nodesMatch g r (fromJust $ target g ge) (fromJust $ target r re) )
        
-matchRuleNode :: GPHostGraph -> GPRuleGraph -> GraphRuleNodeId -> NodeMatches
+matchRuleNode :: HostGraph -> RuleGraph -> RuleNodeId -> NodeMatches
 matchRuleNode g r rn =
     [ (rn, n) | n <- allNodes g , nodesMatch g r n rn ]
 
-matchRuleEdge :: GPHostGraph -> GPRuleGraph -> GraphRuleEdgeId -> EdgeMatches
+matchRuleEdge :: HostGraph -> RuleGraph -> RuleEdgeId -> EdgeMatches
 matchRuleEdge g r re =
     [ (re, e) | e <- allEdges g , edgesMatch g r e re ]
 
-matchNodes :: GPHostGraph -> GPRuleGraph -> NodeMatches
+matchNodes :: HostGraph -> RuleGraph -> NodeMatches
 matchNodes g r = concatMap ( matchRuleNode g r ) $ allNodes r
 
-matchEdges :: GPHostGraph -> GPRuleGraph -> EdgeMatches
+matchEdges :: HostGraph -> RuleGraph -> EdgeMatches
 matchEdges g r = concatMap ( matchRuleEdge g r ) $ allEdges r
 {-matchEdges g r (rn, gn) = union ins outs
     where
@@ -74,7 +63,7 @@ matchEdges g r = concatMap ( matchRuleEdge g r ) $ allEdges r
         outs = filter (uncurry $ edgesMatch g r)
                     [ (re, e) | e <- outEdges g gn, re <- outEdges r rn ] -}
 
-matchGraph :: GPHostGraph -> GPRuleGraph -> [GraphMorphism]
+matchGraph :: HostGraph -> RuleGraph -> [GraphMorphism]
 matchGraph g r = [ (nm, em) | nm <- matchNodes g r , em <- matchEdges g r ]
 
                 
