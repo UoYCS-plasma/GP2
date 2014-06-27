97d18bfbba5d25ed96ee71abf37bbd8f373fb8c4
G Faulkner <glyn.faulkner@york.ac.uk>
2014-06-27 15:13:51 +0100

    Fixed a couple of minor bugs

--- Haskell/FindGraphNode.hs
+++ Haskell/FindGraphNode.hs
@@ -1,16 +1,18 @@
 module FindGraphNode where
 
+import Data.Maybe
+
 import GPSyntax
 import Graph
 
 
-idToNodeId :: RuleGraph -> ID -> NodeID
+idToNodeId :: RuleGraph -> ID -> NodeId
 idToNodeId g id = case candidates of
         [] -> error $ "ID " ++ id ++ " not found"
         [nid] -> nid
         _  -> error $ "Duplicate ID found! Eep!"
     where
-        candidates = filter matchID $ allNodes g
+        candidates = filter (matchID . fromJust . nLabel g) $ allNodes g
         matchID :: RuleNode -> Bool
         matchID (RuleNode i _ _) = i == id
 
