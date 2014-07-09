b6f7fef077c0f9c05458cfeeb03d72e787548545
Chris Bak <chrisbak@cs.york.ac.uk>
2014-07-09 23:20:43 +0100

    Finished adding program execution data to RunProgram and incorporated new output into Main. Typechecks but untested.

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -15,29 +15,36 @@ import PrintGraph
 import ProcessAst
 import RunProgram
 
-horizon = 3
-
 printGraph :: HostGraph -> String
 printGraph = graphToGP2 . makePrintableGraph
 
-printGraphData :: String -> Int -> [(Int, HostGraph)] -> IO ()
+printResult :: FilePath -> Result -> IO ()
+printResult fileName (gs, fc, uc) = do
+   putStrLn $ show fc ++ " fails."
+   putStrLn $ show uc ++ " unfinished computations."
+   printGraphData fileName 1 gs
+
+printGraphData :: FilePath -> Int -> [GraphData] -> IO ()
 printGraphData _ _ [] = putStrLn ""
-printGraphData fileName k ((count,graph):xs) = do
+printGraphData fileName k ((graph, count):gcs) = do
     let outputFile = fileName ++ show k 
     writeFile outputFile $ printGraph graph
     if count == 1 
         then putStrLn $ "1 occurrence of the graph in file " ++ outputFile
         else putStrLn $ show count ++ " occurrences of the graph in file " ++ outputFile 
-    printGraphData fileName (k+1) xs
+    printGraphData fileName (k+1) gcs
 
+-- TODO: convert progFile into a string of the actual program name
+-- i.e. trim off the file extension.
 main = do
-    [progFile, graphFile] <- getArgs
+    [progFile, graphFile, max] <- getArgs
     p <- readFile progFile
     g <- readFile graphFile
+    let maxRules = read max
     let host = makeHostGraph $ parse hostGraph g
     let (prog, syms) = makeGPProgram $ parse program p
-    let pHost = makePrintableGraph host
-    putStrLn $ printGraph host ++ "\n"
-    let graphData = isomorphismCount $ runProgram prog host horizon
-    printGraphData progFile 1 graphData
+    putStrLn $ "GP 2 program " ++ progFile ++ " executed on host graph in " ++ graphFile ++ "."
+    putStrLn $ "Program execution stopped at " ++ show maxRules ++ " rule applications.\n"
+    let result = runProgram prog maxRules host 
+    printResult progFile result
 
--- Haskell/RunProgram.hs
+++ Haskell/RunProgram.hs
@@ -5,13 +5,22 @@ import Graph (emptyGraph)
 import GraphIsomorphism
 import GPSyntax
 
--- Represents a state in the program execution.
--- First component is the current graph.
--- Second component is the number of isomorphic copies of the current graph 
--- encountered during program execution.
--- Third component is the number of rules applied to reach the graph state.
-data GraphState = GS (HostGraph, Int, Int)
+-- GS represents the state of a graph during the program execution.
+-- First component is the working graph.
+-- Second component is the number of isomorphic copies of the working graph 
+-- generated during program execution.
+-- Third component is the number of rules applied to reach the graph state.	
+data GraphState = GS HostGraph Int Int
                 | Failure 
+                | Unfinished
+
+-- A host graph and its isomorphism count.				
+type GraphData = (HostGraph, Int)
+ 
+-- The output of the GP 2 interpreter is a list of output graphs, each with an 
+-- isomorphism count; a failure count; and an unfinished execution count with
+-- respect to the bound on rule applications. 
+type Result = ([GraphData], Int, Int)
 
 -- Output: List of (finished) host graphs, number of unfinished executions.
 -- Failureed executions produce the empty graph. # failures = # empty graphs.
@@ -19,55 +28,57 @@ data GraphState = GS (HostGraph, Int, Int)
 --           Note that this is done per graph, not globally. How to stop execution for one graph?
 -- TODO: Filter out isomorphic graphs at this stage and keep a count.
 
-runProgram :: GPProgram -> Int -> HostGraph -> [GraphState]
-runProgram (Program ds) max g = evalMain max ds (findMain ds) (GS (g, 1, 0))
+runProgram :: GPProgram -> Int -> HostGraph -> Result
+runProgram (Program ds) max g = processData $ evalMain max ds (findMain ds) g
+
+processData :: [GraphState] -> Result
+processData = foldr addGraphState ([], 0, 0)
+   where addGraphState :: GraphState -> Result -> Result
+         addGraphState (GS g ic r) (gs, fc, uc) = ((g, ic):gs, fc, uc)
+         addGraphState (Unfinished) (gs, fc, uc) = (gs, fc, uc+1)
+         addGraphState (Failure) (gs, fc, uc) = (gs, fc+1, uc)
         
 findMain :: [Declaration] -> Main
 findMain ((MainDecl m):ds) = m
 findMain (_:ds) = findMain ds
 findMain [] = error "No main procedure defined."
 
-evalMain :: Int -> [Declaration] -> Main -> GraphState -> [GraphState]
-evalMain max ds (Main coms) gs = evalCommandSequence max ds coms gs
+evalMain :: Int -> [Declaration] -> Main -> HostGraph -> [GraphState]
+evalMain max ds (Main coms) g = evalCommandSequence max ds coms (GS g 1 0)
 
 evalCommandSequence :: Int -> [Declaration] -> [Command] -> GraphState -> [GraphState]
+evalCommandSequence _ _ _ Failure = [Failure]
+evalCommandSequence _ _ _ Unfinished = [Unfinished]
 evalCommandSequence max ds [] gs = [gs]
 evalCommandSequence max ds (c:cs) gs =
     case evalCommand max ds c gs of 
+        [Unfinished] -> [Unfinished]
         [Failure] -> [Failure]
-        hs -> concatMap (evalCommandSequence max ds cs) unfin ++ fin
-              where (fin, unfin) = filterGSs max hs
-
--- Partition the list of graph states into those that have reached the 
--- bound and those that have not.
-filterGSs :: Int -> [GraphState] -> ([GraphState], [GraphState])
-filterGSs _ [] = ([], [])
-filterGSs max (Failure:gss) = 
-    (Failure:fin, unfin) where (fin, unfin) = filterGSs max gss
-filterGSs max (gs:gss)  = 
-    let (fin, unfin)    = filterGSs max gss
-        (GS (_, _, rc)) = gs in
-    if rc == max 
-        then (gs:fin, unfin)         
-        else (fin, gs:unfin) 
-
+        hs -> concatMap (evalCommandSequence max ds cs) hs
 
 evalCommand :: Int -> [Declaration] -> Command -> GraphState -> [GraphState]
+evalCommand _ _ _ Failure = [Failure]
+evalCommand _ _ _ Unfinished = [Unfinished]
 evalCommand max ds (Block b) gs = evalBlock max ds b gs 
 evalCommand max ds (IfStatement cond pass fail) gs = 
     case evalBlock max ds cond gs of 
+        [Unfinished] -> [Unfinished]
         [Failure] -> evalBlock max ds fail gs
         _        -> evalBlock max ds pass gs
 evalCommand max ds (TryStatement cond pass fail) gs = 
     case evalBlock max ds cond gs of
+        [Unfinished] -> [Unfinished]
         [Failure] -> evalBlock max ds fail gs
         hs       -> concatMap (evalBlock max ds pass) hs
 
 
 evalBlock :: Int -> [Declaration] -> Block -> GraphState -> [GraphState]
+evalBlock _ _ _ Failure = [Failure]
+evalBlock _ _ _ Unfinished = [Unfinished]
 evalBlock max ds (ComSeq cs) gs = evalCommandSequence max ds cs gs
 evalBlock max ds (LoopedComSeq cs) gs = 
     case evalCommandSequence max ds cs gs of
+        [Unfinished] -> [Unfinished]
         [Failure] -> [Failure]
         hs     -> concatMap (evalCommandSequence max ds cs) hs
 evalBlock max ds (SimpleCommand sc) gs = evalSimpleCommand max ds sc gs
@@ -76,27 +87,35 @@ evalBlock max ds (ProgramOr b1 b2) gs = evalBlock max ds b1 gs
 
 evalSimpleCommand :: Int -> [Declaration] -> SimpleCommand -> GraphState -> [GraphState]
 evalSimpleCommand _ _ _ Failure = [Failure]
-evalSimpleCommand max ds (RuleCall rs) (GS (g, ic, rc)) = 
-    -- Apply all rules in the set at the same time.
-    let resultGraphs = [h | r <- rs, h <- applyRule g $ ruleLookup r ds] in
-    case resultGraphs of
-        [] -> [Failure]
-        hs -> [GS (meld h (rc+1)) | h <- getIsomorphismData (g, ic) hs]
-              where meld (x, y) z = (x, y, z)
-evalSimpleCommand max ds c@(LoopedRuleCall rs) (GS (g, ic, rc)) = 
-    case evalSimpleCommand max ds (RuleCall rs) (GS (g, ic, rc)) of
-        -- Loop terminates, return input GraphState
-        [] -> [GS (g, ic, rc)]
-        -- One rule call successful. If the bound has been reached, stop and return hs,
-        -- otherwise continue with the loop.
-        hs -> if rc+1 == max then hs else concatMap (evalSimpleCommand max ds c) hs
+evalSimpleCommand _ _ _ Unfinished = [Unfinished]
+evalSimpleCommand max ds (RuleCall rs) (GS g ic rc) = 
+    if rc == max 
+        then [Unfinished]
+        -- Apply all rules in the set at the same time.
+        else let resultGraphs = [h | r <- rs, h <- applyRule g $ ruleLookup r ds] in
+            case resultGraphs of
+                [] -> [Failure]
+                hs -> [makeGS h (rc+1) | h <- getIsomorphismData (g, ic) hs]
+                    where makeGS (x, y) z = GS x y z
+evalSimpleCommand max ds c@(LoopedRuleCall rs) gs@(GS g ic rc) = 
+    if rc == max 
+        then [Unfinished]
+        else 
+            case evalSimpleCommand max ds (RuleCall rs) gs of
+                -- Loop terminates, return input GraphState
+                [Unfinished] -> [Unfinished]
+                [Failure] -> [gs]
+                -- One rule call successful. If the bound has been reached, stop and return hs,
+                -- otherwise continue with the loop.
+                hs -> concatMap (evalSimpleCommand max ds c) hs
 evalSimpleCommand max ds (ProcedureCall proc) gs = evalCommandSequence max (decls++ds) cs gs
     where Procedure id decls cs = procLookup proc ds
 evalSimpleCommand max ds c@(LoopedProcedureCall proc) gs = 
     case evalSimpleCommand max ds (ProcedureCall proc) gs of
-        [Failure] -> [Failure]
+        [Unfinished] -> [Unfinished]
+        [Failure] -> [gs]
         hs     -> concatMap (evalSimpleCommand max ds c) hs
-evalSimpleCommand max ds Skip (GS (g, ic, rc)) = [GS (g, ic, rc+1)]
+evalSimpleCommand max ds Skip (GS g ic rc) = [GS g ic (rc+1)]
 evalSimpleCommand max ds Fail _ = [Failure]
 
 procLookup :: ProcName -> [Declaration] -> Procedure
