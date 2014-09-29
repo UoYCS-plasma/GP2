8cc1b126255aa016a871862d9e0e80886268e36c
G Faulkner <glyn.faulkner@york.ac.uk>
2014-09-29 14:24:27 +0100

    Imported host graphs for benchmarks. Added a makefile to run benchmarks and added .host.d output directories to gitignore

--- Haskell/Benchmarks/benchmark.sh
+++ Haskell/Benchmarks/benchmark.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 GPOPTS=""
-GP="time ../../../Main"
+GP="time -o test.log -a ../../../Main"
 
 for b in */ ; do
-	cd "$b"
-	for host in *.host ; do
+	pushd "$b"
+	prog=`ls *.gp2`
+	for host in `ls *.host` ; do
 		rm -rf "$host.d"
 		mkdir "$host.d"
 		pushd "$host.d"
-		$GP ../*.gp2 ../"$host" >> ../test.log
+		$GP ../"$prog" ../"$host" 20 >> test.log
 		popd
 	done
+	popd
 done
