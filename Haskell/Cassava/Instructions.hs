5972bf31d11739173499d6de4e6fa794b2b3aae5
G Faulkner <glyn.faulkner@york.ac.uk>
2014-10-28 12:07:31 +0000

    Started work on Cassava implmentation

---
+++ Haskell/Cassava/Instructions.hs
@@ -0,0 +1,82 @@
+module Cassava.Instructions where
+
+
+type Addr = Int
+type Deg  = Int
+
+data Instr = 
+    ----------------------------------
+    --  Node and edge finding prims
+    ----------------------------------
+
+    -- Find non-interface nodes
+      NFI Deg
+    | NFO Deg
+    | NFL Deg
+
+    -- Find interface nodes
+    | INFI Deg
+    | INFO Deg
+    | INFL Deg
+
+    -- Find root non-interface nodes
+    | RNFI Deg
+    | RNFO Deg
+    | RNFL Deg
+
+    -- Find root interface nodes
+    | RINFI Deg
+    | RINFO Deg
+    | RINFL Deg
+
+    -- Get the next node
+    | NEXT
+
+    -- Find an edge from 2 nodes
+    | EF
+
+    -- Find two nodes from an edge
+    | NFE
+
+
+    ----------------------------------
+    -- Program control prims
+    ----------------------------------
+
+    | JUMP Addr
+    | JS Addr
+    | JF Addr
+
+
+    ----------------------------------
+    -- Stack management prims
+    ----------------------------------
+
+    | NDUP
+    | NSWAP
+    | NNIP
+    | NDROP
+
+    | EDUP
+    | ESWAP
+    | ENIP
+    | EDROP
+
+    | LDUP
+    | LSWAP
+    | LNIP
+    | LDROP
+
+    | TDUP
+    | TSWAP
+    | TNIP
+    | TDROP
+
+    | GDUP
+    | GSWAP
+    | GNIP
+    | GDROP
+
+    deriving (Show, Eq)
+
+
