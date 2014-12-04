b4e23a566cc92bbce40edff232d883fbceb7e89b
G Faulkner <glyn.faulkner@york.ac.uk>
2014-12-04 21:50:25 +0000

    Added missing source file required by gp2c

---
+++ Haskell/Cassava/HostCompile.hs
@@ -0,0 +1,32 @@
+module Cassava.HostCompile where
+
+import GPSyntax
+import Cassava.Instructions
+import Cassava.Compile
+
+
+compileHostGraph :: AstHostGraph -> Prog
+compileHostGraph (AstHostGraph ns es) =
+    PROC "_HOST" : concatMap (compileHostNode rmap) ns ++ map (compileHostEdge rmap) es ++ [RET]
+    where
+        rmap = makeRegisterMap 0 ns
+
+makeRegisterMap :: Int -> [HostNode] -> RegisterMap
+makeRegisterMap i ((HostNode id _ _):ns) = (id, i) : (makeRegisterMap (i+1) ns)
+makeRegisterMap _ [] = []
+
+
+compileHostNode :: RegisterMap -> HostNode -> Prog
+compileHostNode rmap (HostNode id root _) = 
+    case (root, lookup id rmap) of
+        (True,  Just i) -> [ NEWN, ROOT i ]
+        (False, Just i) -> [ NEWN ]
+        _               -> error "Referenced node is not in register map!"
+
+compileHostEdge :: RegisterMap -> HostEdge -> Instr
+compileHostEdge rmap (HostEdge src tgt _) =
+    case (lookup src rmap, lookup tgt rmap ) of
+        (Just s, Just t) -> NEWE s t
+        _                -> error $ "Endpoint of edge not found in host graph!"
+ 
+
