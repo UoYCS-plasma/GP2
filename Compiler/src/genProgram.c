51351396c15d29710389c25e441f1e72e41ae7b3
Chris Bak <cb574@york.ac.uk>
2015-07-08 15:58:39 +0100

    Another small output file fix.

--- Compiler/src/genProgram.c
+++ Compiler/src/genProgram.c
@@ -202,7 +202,7 @@ void generateRuntimeMain(List *declarations, int host_nodes, int host_edges,
    PTF("   printGraph(host, output_file);\n");
    if(output_file == NULL) 
         PTF("   printf(\"Output graph saved to file ../gp2.output.\\n\");\n");
-   else PTF("   printf(\"Output graph saved to file ../%s.\\n\");\n", output_file);
+   else PTF("   printf(\"Output graph saved to file %s.\\n\");\n", output_file);
    PTF("   garbageCollect();\n");
    PTF("   printf(\"Graph changes made: %%d\\n\", graph_change_count);\n");
    PTF("   fclose(output_file);\n");
