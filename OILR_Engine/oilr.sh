5ff8f5da3fdb611fb5ba4cf5c5f4511692f49bff
G Faulkner <glyn.faulkner@york.ac.uk>
2016-02-11 09:26:42 +0000

    Tidied OILR_Engine dir to streamline build and test

---
+++ OILR_Engine/oilr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "`type -t $0`"="file" ]; then
+	SCR_PATH="`type -p $0`"
+else
+	SCR_PATH="`readlink $0`"
+fi
+
+OILR_DIR="`dirname $SCR_PATH`"
+echo $OILR_DIR
+if [ -z "$OILR_DIR" ]; then
+	echo "You should symlink oilr.sh"
+	echo "into your path"
+	exit 1
+fi
+
+OILR="$OILR_DIR/oilr_machine"
+OILR_LIB="$OILR_DIR/lib/*"
+
+echo $OILR_LIB
+exec $OILR $OILR_LIB $@
+
+
