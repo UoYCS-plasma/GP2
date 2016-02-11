9a0d6cc7057c44636f1c6183604dd39b27e5f394
G Faulkner <glyn.faulkner@york.ac.uk>
2016-02-11 13:28:42 +0000

    Fixing the monster, one tiny program at a time...

--- OILR_Engine/runprogs.sh
+++ OILR_Engine/runprogs.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+
+fail () {
+	echo
+	cat /tmp/oilr.out
+	exit 1
+}
+
 for p in Progs/*.oilr ; do
-	./oilr.sh $p > /tmp/oilr.out
+	echo -ne "$p\n  "
+	./oilr.sh $p > /tmp/oilr.out  || fail
 	../Haskell/IsoChecker $p.out /tmp/oilr.out || exit 1
 done
